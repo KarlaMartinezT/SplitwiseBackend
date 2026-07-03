281729ccf75aec75949642c6baa24d3d4dfc97da
Karla <karlamartinez2574@gmail.com>
2026-07-02 19:19:47 -0700

    agregar cuentasController con endpoints con get y post

---
+++ SplitwiseBackend/Controllers/CuentasController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SplitwiseBackend.Data;
+using SplitwiseBackend.Models;
+
+namespace SplitwiseBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuentasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CuentasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // 1. GET: api/Cuentas (Traer todas las cuentas de gastos)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cuenta>>> GetCuentas()
+        {
+            return await _context.Cuentas.ToListAsync();
+        }
+
+        // 2. POST: api/Cuentas (Crear una nueva cuenta, ej: "Cena de ayer")
+        [HttpPost]
+        public async Task<ActionResult<Cuenta>> PostCuenta(Cuenta cuenta)
+        {
+            _context.Cuentas.Add(cuenta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCuentas), new { id = cuenta.Id }, cuenta);
+        }
+    }
+}

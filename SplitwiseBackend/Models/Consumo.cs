7cc6bcc699aad35a07ab3229ce397a28952d8bab
Karla <karlamartinez2574@gmail.com>
2026-07-02 22:49:01 -0700

    mapeo a columnas de la base de datos

--- SplitwiseBackend/Models/Consumo.cs
+++ SplitwiseBackend/Models/Consumo.cs
@@ -1,13 +1,27 @@
-﻿namespace SplitwiseBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SplitwiseBackend.Models
 {
     public class Consumo
     {
+        [Key] //indica llave primaria d la tabla
         public int Id { get; set; }
+
+        [Column("usuario_id")] //mapeo a la columna real d la bd
         public int IdUsuario { get; set; }
+
+        [Column("cuenta_id")] //mapeo a la columna real d la bd
         public int IdCuenta { get; set; }
+
+        [Column("platillo_nombre")]
         public string PlatilloNombre { get; set; } = string.Empty;
         public decimal Precio { get; set; }
+
+        [ForeignKey("IdUsuario")] //indica que la propiedad Usuario es una clave foranea que hace referencia a la propiedad IdUsuario
         public Usuario? Usuario { get; set; }
+
+        [ForeignKey("IdCuenta")] //indica que la propiedad Cuenta es una clave foranea que hace referencia a la propiedad IdCuenta
         public Cuenta? Cuenta { get; set; }
 
     }
--- SplitwiseBackend/Models/Cuenta.cs
+++ SplitwiseBackend/Models/Cuenta.cs
@@ -1,10 +1,18 @@
-﻿namespace SplitwiseBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SplitwiseBackend.Models
 {
     public class Cuenta
     {
+        [Key]
         public int Id { get; set; }
         public string Descripcion { get; set; } = string.Empty;
+
+        [Column ("total_cuenta")] //mapeo a la columna real d la bd
         public decimal TotalCuentas { get; set; }
+
+        [Column("propina_porcentaje")] //mapeo a la columna real d la bd
         public decimal PropinaPorcentaje { get; set; }
 
     }
